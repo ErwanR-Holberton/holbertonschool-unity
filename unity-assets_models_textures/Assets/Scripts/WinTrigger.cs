f385ffea712474dde289b6d77f1ae9aa878676d1
ErwanR-Holberton <6218@holbertonstudents.com>
2024-06-03 21:26:47 +0200

    Task 9

---
+++ unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+
+    public Timer timerScript; // Reference to the Timer script on the player
+    public Text timerText; // Reference to the Text component
+
+    void OnTriggerEnter(Collider other)
+    {
+        timerScript.enabled = false;
+        timerText.color = Color.green;
+    }
+}
