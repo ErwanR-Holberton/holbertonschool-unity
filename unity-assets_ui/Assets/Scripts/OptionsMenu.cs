9e197abe243242b1c74b3a5430d98a333384c460
ErwanR-Holberton <6218@holbertonstudents.com>
2024-06-23 16:32:38 +0200

    Task 8

--- unity-assets_ui/Assets/Scripts/OptionsMenu.cs
+++ unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
 {
+    Toggle toggle;
+
+    public void Start()
+    {
+        toggle = transform.Find("InvertYToggle").GetComponent<Toggle>();
+        toggle.isOn = PlayerPrefs.GetInt("isInverted", 0) == 1;
+    }
     public void Back()
     {
+        SceneManager.LoadScene(PlayerPrefs.GetString("previous_scene"));
+    }
+    public void Apply()
+    {
+        PlayerPrefs.SetInt("isInverted", toggle.isOn ? 1 : 0);
         SceneManager.LoadScene(PlayerPrefs.GetString("previous_scene"));
     }
 }
