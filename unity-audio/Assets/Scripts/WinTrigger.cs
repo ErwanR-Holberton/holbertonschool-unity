a0a2e4e2ed8179d646e4514d70511f0c56ba721f
ErwanR-Holberton <6218@holbertonstudents.com>
2024-07-31 13:46:29 +0200

    Task 5

--- unity-audio/Assets/Scripts/WinTrigger.cs
+++ unity-audio/Assets/Scripts/WinTrigger.cs
@@ -9,12 +9,15 @@ public class WinTrigger : MonoBehaviour
     private Timer timerScript; // Reference to the Timer script on the player
     private Text timerText; // Reference to the Text component
     public GameObject WinCanvas;
+    private GameObject BGM;
 
     void OnTriggerEnter(Collider other)
     {
         timerScript = GameObject.Find("Player").GetComponent<Timer>();
         timerText = GameObject.Find("TimerText").GetComponent<Text>();
         PauseMenu pauseMenu = GameObject.Find("Player").GetComponent<PauseMenu>();
+        BGM = GameObject.Find("BGM");
+        BGM.SetActive(false);
         pauseMenu.enabled = false;
         Time.timeScale = 0f;
         WinCanvas.SetActive(true);
