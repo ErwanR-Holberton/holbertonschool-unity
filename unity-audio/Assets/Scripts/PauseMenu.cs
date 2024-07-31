828deb8ea65e0a393845f792270c0a0290fa27bd
ErwanR-Holberton <6218@holbertonstudents.com>
2024-07-31 16:09:23 +0200

    Task 8

--- unity-audio/Assets/Scripts/PauseMenu.cs
+++ unity-audio/Assets/Scripts/PauseMenu.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class PauseMenu : MonoBehaviour
 {
     public Timer timerScript;
     public GameObject canvas;
     private bool pause = false;
+    [SerializeField] private AudioMixerSnapshot snapshot_normal, snapshot_pause;
 
     public void Start()
     {
         Time.timeScale = 1f;
+        snapshot_normal.TransitionTo(0);
     }
 
     void Update()
@@ -32,6 +35,7 @@ public class PauseMenu : MonoBehaviour
         timerScript.enabled = false;
         canvas.SetActive(true);
         pause = true;
+        snapshot_pause.TransitionTo(0);
     }
     public void Resume()
     {
@@ -39,13 +43,16 @@ public class PauseMenu : MonoBehaviour
         timerScript.enabled = true;
         canvas.SetActive(false);
         pause = false;
+        snapshot_normal.TransitionTo(0);
     }
     public void Restart()
     {
+        snapshot_normal.TransitionTo(0);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     public void MainMenu()
     {
+        snapshot_normal.TransitionTo(0);
         SceneManager.LoadScene("MainMenu");
     }
     public void Options()
